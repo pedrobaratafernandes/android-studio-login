03a1d012457e81c82735b2ffa15bca0d6d3c9d46
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2025-04-10 20:49:10 +0100

    OutlinedTextField for email and password with spacer

--- app/src/main/java/com/example/login/LoginScreen.kt
+++ app/src/main/java/com/example/login/LoginScreen.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,9 +35,21 @@ fun LoginScreen() {
             modifier = Modifier.size(200.dp)
         )
 
-        Spacer(modifier = Modifier.height(4.dp))
+
 
         Text(text = "Welcome Back!", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Email address")
+        })
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Password")
+        })
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 
 }
