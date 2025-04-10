44f43b5d8ff8d0f226a59a8de2ee2646352a6607
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2025-04-10 20:23:30 +0100

    resize image 200.dp

--- app/src/main/java/com/example/login/LoginScreen.kt
+++ app/src/main/java/com/example/login/LoginScreen.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview(showBackground = true)
 @Composable
@@ -20,7 +22,11 @@ fun LoginScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(painter = painterResource(R.drawable.login), contentDescription = "Login Image")
+        Image(
+            painter = painterResource(R.drawable.login), contentDescription = "Login Image",
+            modifier = Modifier.size(200.dp)
+        )
+
         Text(text = "Welcome Back!")
     }
 
