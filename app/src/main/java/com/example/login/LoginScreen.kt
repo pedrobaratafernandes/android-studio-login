1d3f6d1bfd748d8e515e035ac79ed5c78426fc5a
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2025-04-10 20:26:26 +0100

    add Spacer to separate image from text

--- app/src/main/java/com/example/login/LoginScreen.kt
+++ app/src/main/java/com/example/login/LoginScreen.kt
@@ -3,7 +3,9 @@ package com.example.login
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,6 +30,8 @@ fun LoginScreen() {
             painter = painterResource(R.drawable.login), contentDescription = "Login Image",
             modifier = Modifier.size(200.dp)
         )
+
+        Spacer(modifier = Modifier.height(4.dp))
 
         Text(text = "Welcome Back!", fontSize = 28.sp, fontWeight = FontWeight.Bold)
     }
