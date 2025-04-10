27e9ae54da4ac009c69395d0eda59401033a305e
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2025-04-10 21:03:24 +0100

    Finalize LoginScreen.kt

--- app/src/main/java/com/example/login/LoginScreen.kt
+++ app/src/main/java/com/example/login/LoginScreen.kt
@@ -1,6 +1,8 @@
 package com.example.login
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,13 +12,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,6 +27,13 @@ import androidx.compose.ui.unit.sp
 @Preview(showBackground = true)
 @Composable
 fun LoginScreen() {
+
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -35,31 +45,43 @@ fun LoginScreen() {
             modifier = Modifier.size(200.dp)
         )
 
-
-
         Text(text = "Welcome Back!", fontSize = 28.sp, fontWeight = FontWeight.Bold)
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(text = "Login to your account", fontSize = 14.sp)
+
         Spacer(modifier = Modifier.height(10.dp))
-        OutlinedTextField(value = "", onValueChange = {}, label = {
+
+        OutlinedTextField(value = email, onValueChange = {
+            email = it
+        }, label = {
             Text(text = "Email address")
         })
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(value = "", onValueChange = {}, label = {
+        OutlinedTextField(value = password, onValueChange = {
+            password = it
+        }, label = {
             Text(text = "Password")
-        })
+        }, visualTransformation = PasswordVisualTransformation())
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Button(onClick = { }) {
+            Log.i("Crendentials", "Email: $email, Password: $password")
             Text(text = "Login")
         }
+
         Spacer(modifier = Modifier.height(32.dp))
-        Text(text = "Forgot Password?", fontSize = 14.sp)
+
+        TextButton(onClick = { }) {
+            Text(text = "Forgot Password ?", modifier = Modifier.clickable {
+
+            })
+        }
+
     }
 
 }
